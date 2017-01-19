7434ae1cecf101849868c18633792eecd7a971b9
Bastian Schade <rumtraubenuss@googlemail.com>
2017-01-19 09:31:38 +0100

    Add env conditional api url

--- src/PriceList.js
+++ src/PriceList.js
@@ -10,14 +10,21 @@ class PriceList extends Component {
   }
 
   componentDidMount() {
-    fetch('https://coinster-api.herokuapp.com/api/prices')
+    let API_URL = '';
+    if(process.env.NODE_ENV === 'production') {
+      API_URL = 'https://coinster-api.herokuapp.com/api/prices';
+    } else {
+      API_URL = 'http://localhost:8000/api/prices';
+    }
+    fetch(API_URL)
       .then(res => res.json())
       .then(json => { this.setState({ prices: json }) });
   }
 
   render() {
+    console.log(moment.locale());
     const list = this.state.prices.map((price, count) => {
-      const date = moment(price.date).calendar();
+      const date = moment(price.date).format('ll H:mm:ss');
       return <li key={count}>{price.price_euro} {date}</li>;
     });
     return(
