85d753f99a6b55c15b4e8a3e970bfb522a6d2b39
Bastian Schade <rumtraubenuss@googlemail.com>
2017-04-17 18:19:10 +0200

    Calculate and print 24h trend of price

--- src/PriceList.js
+++ src/PriceList.js
@@ -3,6 +3,7 @@ import './PriceList.css';
 import { FormattedRelative } from 'react-intl';
 import { Panel, Button, Well, Grid, Row, Col } from 'react-bootstrap';
 import moment from 'moment';
+import last from 'ramda/src/last';
 
 class PriceList extends Component {
   constructor(props) {
@@ -69,7 +70,11 @@ class PriceList extends Component {
         </span>
     }
     const list = Object.keys(prices).map((type, count) => {
-      const price = this.state.prices[type][0];
+      const { prices } = this.state;
+      const priceLast = last(prices[type]);
+      const price = prices[type][0];
+      const trendPercent = ((price.price / priceLast.price - 1) * 100).toFixed(2);
+      const trendDirection = trendPercent >= 0 ? '+' : '-';
       return (
         <Panel key={count} header={price.type}>
           <Grid fluid>
@@ -78,7 +83,7 @@ class PriceList extends Component {
                 <strong>{price.price.toFixed('2')} </strong>
                 <small>{moment(price.date).calendar()}</small>
               </Col>
-              <Col xs={4}><small>-24h:</small></Col>
+              <Col xs={4}><small>24h: {trendDirection}{trendPercent}%</small></Col>
             </Row>
           </Grid>
         </Panel>
